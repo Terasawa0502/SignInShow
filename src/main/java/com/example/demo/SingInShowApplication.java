797b3fbc9d4c6772f2cb63b61801c60e6efaa8a1
寺澤徳隆 <mosqey1889@gmail.com>
2024-04-14 10:24:11 +0900

    FirsrCommit

---
+++ src/main/java/com/example/demo/SingInShowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingInShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingInShowApplication.class, args);
+	}
+
+}
